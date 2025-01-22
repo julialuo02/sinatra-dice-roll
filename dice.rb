feba4454c21b78085cba75aaca88cae93345f284
julialuo02 <121737951+julialuo02@users.noreply.github.com>
2025-01-22 16:20:30 +0000

    finished dice

--- dice.rb
+++ dice.rb
@@ -8,6 +8,10 @@ BetterErrors.application_root = __dir__
 BetterErrors::Middleware.allow_ip!('0.0.0.0/0.0.0.0')
 
 get("/") do
+  erb(:elephant)
+end
+
+get("/") do
   "Hello World"
 end
 
@@ -23,31 +27,30 @@ get("/dice/2/6") do
   first_die = rand(1..6)
   second_die = rand(1..6)
   sum = first_die + second_die
-	
-  outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
-	
-  "<h1>2d6</h1>
-   <p>#{outcome}</p>"
+
+  @outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
+
+  erb(:two_six)
 end
 
 get("/dice/2/10") do
   first_die = rand(1..10)
   second_die = rand(1..10)
   sum = first_die + second_die
-	
-  outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
-	
-  "<h1>2d6</h1>
-   <p>#{outcome}</p>"
+
+  @outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
+
+  erb(:two_ten)
 end
 
+# /dice.rb
+
 get("/dice/1/20") do
-  first_die = rand(1..20)
-	
-  outcome = "You rolled a #{first_die}."
-	
-  "<h1>2d6</h1>
-   <p>#{outcome}</p>"
+  @die = rand(1..20)
+
+  @outcome = "You rolled a #{@die}."
+
+  erb(:one_twenty)
 end
 
 get("/dice/5/4") do
@@ -58,8 +61,19 @@ get("/dice/5/4") do
   fifth_die = rand(1..4)
   sum = first_die + second_die + third_die + fourth_die + fifth_die
 	
-  outcome = "You rolled a #{first_die}, a #{second_die}, a #{third_die}, a #{fourth_die}, and a #{fifth_die} for a total of #{sum}."
+  @outcome = "You rolled a #{first_die}, a #{second_die}, a #{third_die}, a #{fourth_die}, and a #{fifth_die} for a total of #{sum}."
 	
-  "<h1>2d6</h1>
-   <p>#{outcome}</p>"
+  erb(:five_four)
+end
+
+get("/dice/100/6") do
+  @rolls = []    # Create a blank array
+
+  100.times do    # 100 times...
+    die = rand(1..6)    # Generate a random number
+
+    @rolls.push(die)    # Add the random number to the array 
+  end
+
+  erb(:one_hundred_six)
 end
